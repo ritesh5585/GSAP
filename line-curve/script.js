69f77b0bd9b656d849c867474f984c0d497276e7
ritesh vishwakarma <vishwakarmaritesh5585.com>
2026-05-11 00:11:07 +0530

    curve line animation on cursor movements

---
+++ line-curve/script.js
@@ -0,0 +1,25 @@
+var initialPath = `M 10 100 Q 500 100 990 100`
+var finalPath = `M 10 100 Q 500 100 990 100`
+
+let main = document.querySelector(".main")
+
+main.addEventListener("mousemove", function (dets) { 
+    path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
+
+    gsap.to("svg path", {
+        attr: {d: path},
+        duration: 0.2,
+        ease: "power2.out"
+    })
+})
+
+main.addEventListener("mouseleave", function(){
+
+    gsap.to("svg path",{
+        attr: {d: finalPath},
+        duration: 1.2,
+        ease: "elastic.out(1,0,2)"
+    })
+})
+
+
