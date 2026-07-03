80459aff7207340fc79cd73c23f6cfa512609705
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-07-03 15:19:08 +0530

    understanding stagger

--- Gsap with React/src/App.jsx
+++ Gsap with React/src/App.jsx
@@ -9,15 +9,21 @@ gsap.set(".box1",{
 
     gsap.to(".box1", {
       x: 1200,
-      duration: 1.3,
+      duration: 1,
       delay: 0.5,
-      ease: "powder2.inOut(0.7)",
-      repeat:5
+      ease: "powder2.out(0.7)",
+      repeat:5,
+      stagger: 0.3
     });
   }, []);
 
   return (
     <div>
+      <span className="box1"></span>
+      <span className="box1"></span>
+      <span className="box1"></span>
+      <span className="box1"></span>
+      <span className="box1"></span>
       <span className="box1"></span>
     </div>
   );
