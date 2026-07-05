cff73782f2a2450d463b7dbdf1f23de0023d27dd
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-07-05 23:08:17 +0530

    trying stragers

--- Gsap with React/src/App.jsx
+++ Gsap with React/src/App.jsx
@@ -3,28 +3,28 @@ import gsap from "gsap";
 
 export default function App() {
   useEffect(() => {
-gsap.set(".box1",{
-  x: -300
-})
+    gsap.set(".box1", {
+      // x: -200,
+    });
 
     gsap.to(".box1", {
       x: 1200,
       duration: 1,
       delay: 0.5,
       ease: "powder2.out(0.7)",
-      repeat:5,
-      stagger: 0.3
+      repeat: 5,
+      stagger: 0.2,
     });
   }, []);
 
   return (
-    <div>
-      <span className="box1"></span>
-      <span className="box1"></span>
-      <span className="box1"></span>
-      <span className="box1"></span>
-      <span className="box1"></span>
-      <span className="box1"></span>
+    <div className="box">
+      <span className="box1">R</span>
+      <span className="box1">I</span>
+      <span className="box1">T</span>
+      <span className="box1">E</span>
+      <span className="box1">S</span>
+      <span className="box1">H</span>
     </div>
   );
 }
