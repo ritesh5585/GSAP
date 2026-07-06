15503777db42572cc60b2b06fb53a11988b0431a
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-07-06 13:34:50 +0530

    Refactor animation setup and practing...

--- Gsap with React/src/App.jsx
+++ Gsap with React/src/App.jsx
@@ -7,27 +7,44 @@ export default function App() {
       // x: -200,
     });
 
-    gsap.to(".box1", {
-      x: 800,
-      duration: 1,
-      delay: 0.5,
-      ease: "powder2.out(0.7)",
+    // gsap.to(".box1", {
+    //   x: 800,
+    //   duration: 1,
+    //   delay: 0.5,
+    //   ease: "powder2.out(0.7)",
+    //   repeat: 5,
+    //   stagger: {
+    //     each: 0.1,
+    //     from: "center"
+    //   },
+    // });
+
+    gsap.from("h1 span", {
+      yPercent: 100,
+      delay: 1,
+      opacity: 0,
+      duration: 0.7,
+      ease: "expo.in",
       repeat: 5,
       stagger: {
-        each: 0.1,
-        from: "edges"
+        each: 0.08,
+        from: "start",
       },
     });
   }, []);
 
   return (
     <div className="box">
-      <span className="box1">R</span>
-      <span className="box1">I</span>
-      <span className="box1">T</span>
-      <span className="box1">E</span>
-      <span className="box1">S</span>
-      <span className="box1">H</span>
+      <div className="box1">
+        <h1>
+          <span className="box1">R</span>
+          <span className="box1">I</span>
+          <span className="box1">T</span>
+          <span className="box1">E</span>
+          <span className="box1">S</span>
+          <span className="box1">H</span>
+        </h1>
+      </div>
     </div>
   );
 }
