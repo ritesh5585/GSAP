6a35c751f51f552d68981c9cf5585eb2facc7776
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-07-16 22:50:51 +0530

    Remove unused GSAP animations and related styles from App component

--- Gsap with React/src/App.jsx
+++ Gsap with React/src/App.jsx
@@ -2,49 +2,12 @@ import React, { useEffect } from "react";
 import gsap from "gsap";
 
 export default function App() {
-  useEffect(() => {
-    gsap.set(".box1", {
-      // x: -200,
-    });
-
-    // gsap.to(".box1", {
-    //   x: 800,
-    //   duration: 1,
-    //   delay: 0.5,
-    //   ease: "powder2.out(0.7)",
-    //   repeat: 5,
-    //   stagger: {
-    //     each: 0.1,
-    //     from: "center"
-    //   },
-    // });
-
-    gsap.from("h1 span", {
-      yPercent: 100,
-      delay: 1,
-      opacity: 0,
-      duration: 0.7,
-      ease: "expo.in",
-      repeat: 5,
-      stagger: {
-        each: 0.08,
-        from: "start",
-      },
-    });
-  }, []);
-
+  useEffect(() => {}, []);
   return (
-    <div className="box">
-      <div className="box1">
-        <h1>
-          <span className="box1">R</span>
-          <span className="box1">I</span>
-          <span className="box1">T</span>
-          <span className="box1">E</span>
-          <span className="box1">S</span>
-          <span className="box1">H</span>
-        </h1>
+    <>
+      <div className="container">
+        <button className="btn"></button>
       </div>
-    </div>
+    </>
   );
 }
